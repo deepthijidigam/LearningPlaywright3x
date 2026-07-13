ab32a82dff8dbc305cc8621f04b40a500153e77f
deepthijidigam <deepthijidigam@users.noreply.github.com>
2026-07-13 23:34:14 +0530

    Add literal chapter with 04_chapter_literal.js
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 04_chapter_literal/04_chapter_literal.js
@@ -0,0 +1,13 @@
+let name = 'abcd';
+let Name = 'Guna';
+let pai = 3.14;
+let xyz;
+let careModel = null;
+let fName = "Dhana";
+
+console.log('name:', typeof name);
+console.log('Name:', typeof Name);
+console.log('pai:', typeof pai);
+console.log('xyz:', typeof xyz);
+console.log('carModel:', typeof careModel);
+console.log('fName:', typeof fName);
