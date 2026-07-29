95df206d4e0b5fcc8eabf483e987d5bef3994bdf
deepthijidigam <deepthijidigam@users.noreply.github.com>
2026-07-29 21:28:52 +0530

    Add confusing comparison examples (part 2)

--- 05_chapter_operator/10_confusing_compar2.js
+++ 05_chapter_operator/10_confusing_compar2.js
@@ -6,4 +6,5 @@ console.log(null === undefined);  //f
 console.log(null == 0);  //f
 console.log(null > 0);  //f while comparing null will become 0, so 0>0 is false
 console.log(null >= 0); //t again as above 0>=0 is true 
-console.log(null == 0 || null > 0) // f
+console.log(null == 0 || null > 0) //f
+console.log(typeof null);
